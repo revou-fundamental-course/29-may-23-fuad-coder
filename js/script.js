dc40d562735d2076fdd1bb6b994538ad3c3c36a5
fuad <fuadh4965@gmail.com>
2023-06-11 21:36:26 +0700

    js bisa namun di dalam html

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const text = ("Hello World");
-
-document.getElementById("kirim").addEventListener('click', function() {
-    console.log(text);
-})
-document,getElementById("input-nama").addEventListener('input', function() {
-    const nama = document.getElementById("nama");
-    console.log(nama)
-})
